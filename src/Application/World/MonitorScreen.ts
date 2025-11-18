8ba130bd640baad9b36af5b07f03154f380f9084
unreal! <aimaressi88@gmail.com>
2025-11-18 19:04:59 +0100

    Update MonitorScreen.ts

--- src/Application/World/MonitorScreen.ts
+++ src/Application/World/MonitorScreen.ts
@@ -184,7 +184,7 @@ export default class MonitorScreen extends EventEmitter {
 
         // Set iframe attributes
         // PROD
-        iframe.src = 'os-itzmeunreal.vercel.app';
+        iframe.src = 'https://os-itzmeunreal.vercel.app';
         /**
          * Use dev server is query params are present
          *
